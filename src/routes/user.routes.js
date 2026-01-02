4583bb06d5ba091b56e78bd61841787b311646c6
Vrushabh Kathiriya <kathiriyavrushabh0702@gmail.com>
2026-01-02 20:29:23 +0530

    Add subscription model

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
-import {upload} from "../middlewares/multer.middleware.js"
+import {upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
@@ -41,11 +41,11 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(
   verifyJWT,
-  logoutUser)
-router.route("/refresh-token").post(refreshAccessToken)
+  router.route("/refresh-token").post(refreshAccessToken,
+  logoutUser))
 
 
-
+ 
 
 
 export default router
